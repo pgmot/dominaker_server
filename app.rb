8ea278ef6a8660d3f7a3537b0967ef0b87237431
mot <mot.contact.address.pc@gmail.com>
2015-11-03 19:10:26 +0900

    websocket sslに直した

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ __END__
           return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
         }(document.getElementById('msgs'));
 
-        var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
+        var ws       = new WebSocket('wss://' + window.location.host + window.location.pathname);
         ws.onopen    = function()  { show('websocket opened'); };
         ws.onclose   = function()  { show('websocket closed'); }
         ws.onmessage = function(m) { show('websocket message: ' +  m.data); };
