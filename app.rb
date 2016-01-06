67c4309a6ffab53dc936613a161befae195b3ed4
Kousuke Sugano <sinner.shiki.g@gmail.com>
2016-01-06 17:15:01 +0900

    ユーザを均一にばらまく感じにした

--- app.rb
+++ app.rb
@@ -11,18 +11,31 @@ post '/register' do
   req = JSON.parse(request.body.read).to_hash
   req_uuid = req["uuid"]
   flag = 0
-  puts team_id = [0,1].sample
-  response = {team_id: team_id}
+  num_of_Tteam = redis.get TEAM[0]
+  num_of_Yteam = redis.get TEAM[1]
+  nums = [num_of_Tteam.to_i, num_of_Yteam.to_i]
+  diff = nums[0] - nums[1]
+  if diff == 0
+    @team_id = [0,1].sample
+  elsif diff < 0
+    @team_id = 0
+  else
+    @team_id = 1
+  end
   users = redis.lrange :users, 0, -1
   users.each do |user|
     user = JSON.parse(user).to_hash
-    if req_uuid == user[:uuid]
+    if req_uuid == user["uuid"]
       flag = 1
-      response[:team_id] = user[:team_id]
+      @team_id = user["team_id"]
     end
   end
-  redis.rpush :users, {uuid: req_uuid, team_id: team_id}.to_json if flag == 0
-  response.to_json
+  if flag == 0
+    redis.rpush :users, {uuid: req_uuid, team_id: @team_id}.to_json
+    redis.set TEAM[@team_id], nums[@team_id] += 1
+  end
+
+  {team_id: @team_id}.to_json
 end
 
 get '/' do
